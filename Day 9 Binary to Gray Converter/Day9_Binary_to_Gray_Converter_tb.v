5bc2959e3bfb9265d6055f58adb535ad25dddae1
Dev Chadha <chadhadev2002@gmail.com>
2022-12-29 23:54:58 +0530

    Binary to Gray Converter

--- Day 9 Binary to Gray Converter/Day9_Binary_to_Gray_Converter_tb.v
+++
@@ -1,9 +0,0 @@
-module Day9_Binary_to_Gray_Converter(input [3 : 0]bin_inp,
-                                     output reg [3 : 0]gray_out);
-  
-assign gray_out[3] = bin_inp[3];
-assign gray_out[2] = bin_inp[3] ^ bin_inp[2];
-assign gray_out[1] = bin_inp[2] ^ bin_inp[1];
-assign gray_out[0] = bin_inp[1] ^ bin_inp[0];
-
-endmodule
