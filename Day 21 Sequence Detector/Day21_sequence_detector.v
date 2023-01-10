cb7e25456f1ddb566c6b29cc52f05ef82ec71a41
Dev Chadha <chadhadev2002@gmail.com>
2023-01-10 18:31:57 +0530

    Sequence Detector

---
+++ Day 21 Sequence Detector/Day21_sequence_detector.v
@@ -0,0 +1,20 @@
+module Day21_sequence_detector(input clk, 
+                               input seq, 
+                               output reg detected);
+  
+  reg [1:0] state; // 2-bit register to store the current state
+  
+  initial state <= 2'b00;
+  
+  always @(posedge clk) begin
+    case (state)
+      2'b00: if (seq) state <= 2'b01; else state <= 2'b00;
+      2'b01: if (seq) state <= 2'b01; else state <= 2'b10;
+      2'b10: if (seq) state <= 2'b11; else state <= 2'b00;
+      2'b11: if (seq) state <= 2'b01; else state <= 2'b10;
+    endcase
+    if (state == 2'b11) detected <= 1'b1;
+    else detected <= 1'b0;
+  end
+  
+endmodule
